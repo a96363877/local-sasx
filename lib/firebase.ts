54b6ca5ce27c6fb8a8a1ee416666499281d1c393
a96363877 <a96363877@gmail.com>
2025-02-18 19:29:41 -0800

    ASD

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   // Your Firebase configuration will be injected here
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBSBQlPHwbrkBxeQ54RZw6jGUkfcBAeI-0",
+  authDomain: "moror-7892f.firebaseapp.com",
+  projectId: "moror-7892f",
+  storageBucket: "moror-7892f.firebasestorage.app",
+  messagingSenderId: "644937901522",
+  appId: "1:644937901522:web:e9fcf90e8d6d187f7b140b",
+  measurementId: "G-FEC36WC9S7"
 }
 
 const app = initializeApp(firebaseConfig)
